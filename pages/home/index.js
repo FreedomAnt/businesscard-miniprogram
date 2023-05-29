7777116d6496897c103b070b65d299669b828a58
FreedomAnt <925583849@qq.com>
2023-05-29 22:25:27 +0800

    daily update

--- pages/home/index.js
+++ pages/home/index.js
@@ -35,7 +35,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {},
+  onShow: function () { },
 
   /**
    * 用户点击右上角分享
@@ -52,7 +52,7 @@ Page({
   /**
    * 用户点击右上角分享到朋友圈
    */
-  onShareTimeline: function () {},
+  onShareTimeline: function () { },
 
   /**
    * 获取openid
@@ -276,7 +276,7 @@ Page({
       firstName: name,
       mobilePhoneNumber: phonenumber,
       organization: this.data.company.name,
-      title: this.data.department.name + "." + position,
+      title: position,
       email,
     });
   },
